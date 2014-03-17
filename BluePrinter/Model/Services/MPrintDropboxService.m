7c6750af4c7d9cd834c22ff997b35fbdab641688
David Quesada <davidq2012@gmail.com>
2014-03-17 16:16:06 -0400

    Fix Dropbox auth. Again.

--- BluePrinter/Model/Services/MPrintDropboxService.m
+++ BluePrinter/Model/Services/MPrintDropboxService.m
@@ -30,6 +30,21 @@
     return MPrintNetworkedServiceConnectionMethodAuthenticated;
 }
 
+-(NSURL *)authorizationURL
+{
+    // This is a hack. For some reason (as of March 17), MPrint won't like it if
+    // you try to activate the service by using the callback at /api/services/dropbox,
+    // but will handle /settings/services/dropbox, which is an html frontend. So we
+    // need to modify the URL to use the method that works.
+    NSURL *url = [super authorizationURL];
+    NSString *newString = [[url description] stringByReplacingOccurrencesOfString:@"api%2" withString:@"settings%2"];
+    
+    url = [NSURL URLWithString:newString];
+    NSDebugLog(@"New URL for Dropbox Auth : %@", url);
+    
+    return url;
+}
+
 -(MPrintNetworkedServiceAuthResult)actionForMPrintURL:(NSURL *)url
 {
     NSString *query = url.query;
